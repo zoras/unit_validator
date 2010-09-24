45b0c6087988671e80ac069cdf795d3662d9789a
saroz <saroj@sproutfiy.com>
2010-09-24 15:32:04 +0545

    Rspec setup with growls, Jeweler implemented

---
+++ spec/cloud_factory_validator_spec.rb
@@ -0,0 +1,16 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "CloudFactoryValidator" do
+
+  it "should parse a .csv file" do
+    
+  end
+  
+  it "should validate the csv file headers" do
+    pending
+  end
+  
+  it "should validate the csv file rows against the rules provided by FormFactory gem" do
+    pending
+  end
+end
